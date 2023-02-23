e38813f424094233e809bfc2f00f5468622a30f7
Kudret Davudov <bordobereli435@gmail.com>
2023-02-23 14:46:14 +0200

    changes on ReceiveSharingIntent; remove listener on clearReceivedFiles

--- lib/typescript/ReceiveSharingIntent.d.ts
+++ lib/typescript/ReceiveSharingIntent.d.ts
@@ -2,9 +2,11 @@ import type { IReceiveSharingIntent } from './ReceiveSharingIntent.interfaces';
 declare class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     private isIos;
     private utils;
-    private isClear;
-    getReceivedFiles(handler: Function, errorHandler: Function, protocol?: string): void;
+    private subscription;
+    constructor();
+    getReceivedFiles(handler: Function, errorHandler: Function, protocol?: string): Promise<void>;
     clearReceivedFiles(): void;
-    protected getFileNames(handler: Function, errorHandler: Function, url: string): void;
+    protected getFileNames(handler: Function, errorHandler: Function, url: string): Promise<void>;
 }
 export default ReceiveSharingIntentModule;
+//# sourceMappingURL=ReceiveSharingIntent.d.ts.map
--- src/ReceiveSharingIntent.ts
+++ src/ReceiveSharingIntent.ts
@@ -2,68 +2,83 @@ import type {
   IReceiveSharingIntent,
   IUtils,
 } from './ReceiveSharingIntent.interfaces';
-import { Platform, Linking, AppState, NativeModules } from 'react-native';
+import {
+  Platform,
+  Linking,
+  NativeModules,
+  EmitterSubscription,
+} from 'react-native';
 import Utils from './utils';
 
 const { ReceiveSharingIntent } = NativeModules;
 
 class ReceiveSharingIntentModule implements IReceiveSharingIntent {
-  private isIos: boolean = Platform.OS === 'ios';
-  private utils: IUtils = new Utils();
-  private isClear: boolean = false;
+  private isIos: boolean;
+  private utils: IUtils;
 
-  getReceivedFiles(
+  private subscription: EmitterSubscription | undefined;
+
+  constructor() {
+    this.isIos = Platform.OS === 'ios';
+    this.utils = new Utils();
+
+    this.subscription = undefined;
+  }
+
+  async getReceivedFiles(
     handler: Function,
     errorHandler: Function,
     protocol: string = 'ShareMedia'
   ) {
-    if (this.isIos) {
-      Linking.getInitialURL()
-        .then((res: any) => {
-          if (res && res.startsWith(`${protocol}://dataUrl`) && !this.isClear) {
-            this.getFileNames(handler, errorHandler, res);
+    try {
+      if (this.isIos) {
+        const URL = await Linking.getInitialURL();
+
+        if (URL?.startsWith(`${protocol}://dataUrl`)) {
+          await this.getFileNames(handler, errorHandler, URL);
+        }
+
+        this.subscription = Linking.addEventListener('url', async (res) => {
+          const URL = res ? res.url : '';
+
+          if (URL?.startsWith(`${protocol}://dataUrl`)) {
+            await this.getFileNames(handler, errorHandler, URL);
           }
-        })
-        .catch(() => {});
-      Linking.addEventListener('url', (res: any) => {
-        const url = res ? res.url : '';
-        if (url.startsWith(`${protocol}://dataUrl`) && !this.isClear) {
-          this.getFileNames(handler, errorHandler, res.url);
-        }
-      });
-    } else {
-      AppState.addEventListener('change', (status: string) => {
-        if (status === 'active' && !this.isClear) {
-          this.getFileNames(handler, errorHandler, '');
-        }
-      });
-      if (!this.isClear) this.getFileNames(handler, errorHandler, '');
+        });
+      } else {
+        await this.getFileNames(handler, errorHandler, '');
+      }
+    } catch (error) {
+      errorHandler(error);
     }
   }
 
   clearReceivedFiles() {
     ReceiveSharingIntent.clearFileNames();
+
+    if (this.subscription) {
+      this.subscription.remove();
+      this.subscription = undefined;
+    }
   }
 
-  protected getFileNames(
+  protected async getFileNames(
     handler: Function,
     errorHandler: Function,
     url: string
   ) {
-    if (this.isIos) {
-      ReceiveSharingIntent.getFileNames(url)
-        .then((data: any) => {
-          let files = this.utils.sortData(data);
-          handler(files);
-        })
-        .catch((e: any) => errorHandler(e));
-    } else {
-      ReceiveSharingIntent.getFileNames()
-        .then((fileObject: any) => {
-          let files = Object.keys(fileObject).map((k) => fileObject[k]);
-          handler(files);
-        })
-        .catch((e: any) => errorHandler(e));
+    try {
+      if (this.isIos) {
+        const data = await ReceiveSharingIntent.getFileNames(url);
+        const files = this.utils.sortData(data);
+        handler(files);
+      } else {
+        const fileObject = await ReceiveSharingIntent.getFileNames();
+        const files = Object.keys(fileObject).map((key) => fileObject[key]);
+        handler(files);
+      }
+    } catch (error) {
+      errorHandler(error);
     }
   }
 }
